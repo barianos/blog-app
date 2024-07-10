1e518020c542bc1ba6feff130769f9ae4bb59e1e
Anastasios Barianos <barianos@outlook.com>
2024-07-10 16:19:26 +0300

    add logic to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,14 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'content',
+        'author_id',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
